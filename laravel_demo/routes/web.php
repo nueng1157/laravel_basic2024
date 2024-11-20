cffae31730e89c50d7281f419321218922f41bcd
nueng1157 <nueng1157@gmail.com>
2024-11-20 22:53:30 +0700

    Day4 laravel

--- laravel_demo/routes/web.php
+++ laravel_demo/routes/web.php
@@ -37,3 +37,41 @@ Route::post('/post', function (Request $request){
     $name = $request->name;
     return json_encode (['name'=> $name]);
 });
+
+Route::get('/home', function(){
+    return view('home');
+});
+
+Route::get('/response', function (){
+    return response()->json(['name' => 'Tavon']);
+});
+
+Route::get('/products', function(){
+    $products = [
+        ['id' =>1, 'name' => 'Product 1', 'price'=>100],
+        ['id' =>2, 'name' => 'Product 2', 'price'=>200],
+        ['id' =>3, 'name' => 'Product 3', 'price'=>300],
+    ];
+    return response()->json($products);
+});
+
+Route::get('/response-type', function(){
+    //401 = unthorized
+    //403 = forbidden
+    //404 = not found ใส่ path ไม่ถูก
+    //500 = internal server error  server error
+    //200 = ok
+    //201 = created
+    //202 = accepted
+    //204 = no content
+    return response('Untorized', 401);
+});
+
+Route::get('/rediect', function(){
+    return redirect('/target');
+});
+
+Route::get('/target', function(){
+   return 'Target'; 
+});
+
